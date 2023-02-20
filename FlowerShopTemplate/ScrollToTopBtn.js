b74c3ea4465d2179370a8501091709925ef75dd1
mohammad <mo.se.12369@gmail.com>
2023-02-20 09:28:48 +0330

    #

--- FlowerShopTemplate/ScrollToTopBtn.js
+++ FlowerShopTemplate/ScrollToTopBtn.js
@@ -1,16 +1,21 @@
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById('myBtn')
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    mybutton.style.display = "block";
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    mybutton.style.display = 'block'
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.display = 'none'
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
 }
